8566faffd4a9e659fa39f106f4f1afeb27218d74
bishnunathbd <bishnunath774@gmail.com>
2020-12-18 09:38:41 +0600

    47-8-post data from website, read post api response

--- index.js
+++ index.js
@@ -30,7 +30,10 @@ app.get('/users/:id', (req, res) => {
 
 //post
 app.post('/addUser', (req, res) => {
-  console.log(req.body);
+  // console.log('data received', req.body);
+  const user = req.body;
+  user.id = 55;
+  res.send(user);
 })
 
 app.listen(3000, ()=> console.log('listening to port 3000'))
